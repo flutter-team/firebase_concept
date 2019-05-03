72d0417f90336a13a0e8f9ad740d156d881e1388
sonikapunitgoyal <48379147+sonikapunitgoyal@users.noreply.github.com>
2019-05-03 17:03:17 +0530

    Update resetpassword.dart

--- lib/resetpassword.dart
+++ lib/resetpassword.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'login.dart';
 class ResetPassword extends StatefulWidget {
   
   @override
@@ -19,14 +20,49 @@ return false;
 void validateAndSubmit()async{
   if(validateAndSave()){
   
-    await FirebaseAuth.instance.sendPasswordResetEmail(email: _email);}}
+    try{
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: _email);
+      return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(title:Text('Info') ,
+         content:Text('open ur gmail and change password') ,
+         actions: <Widget>[
+           FlatButton(child: Text('ok'),onPressed: (){
+             Navigator.pop(context);
+           },)
+         ],);});
+    
+    }catch(e){
+     return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(title:Text('Info') ,
+         content:Text('erroe $e') ,
+         actions: <Widget>[
+           FlatButton(child: Text('ok'),onPressed: (){
+             Navigator.pop(context);
+           },)
+         ],);});  
+    }
+    }
+    }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Form(key: _formKey, child: Column(
+    return Scaffold( appBar: AppBar(
+      title: Text('Reset Password'),),
+      body: Form(key: _formKey, child: Column(
       children: <Widget>[
       TextFormField(
-                  decoration: InputDecoration(hintText: 'Email'), 
-                  validator: (val) => val.isEmpty ? 'Password can\'t be empty.' : null,
+                  decoration: InputDecoration(hintText: 'Email',), 
+                   validator: (value) {
+        if (value.isEmpty ||
+            !RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value)) {
+          return 'Please enter a valid email';
+        }
+      },
                   onSaved: (value) {
                     setState(() {
                       _email = value;
